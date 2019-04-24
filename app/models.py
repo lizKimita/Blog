285bc6944a5798271519bbb40e1791a64d2396e3
lizKimita <kimita.wanjiku@gmail.com>
2019-04-24 16:42:50 +0300

    adding class user, roles and post in the models file

--- app/models.py
+++ app/models.py
@@ -5,9 +5,56 @@ from . import login_manager
 from datetime import datetime
 
 
-class Post:
-    '''
-    Post class to define Post Objects
-    '''
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    posts = db.relationship('Post', backref = 'user', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+   
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
     
-    
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255))
+    post = db.Column(db.String())
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_posts(cls):
+        Post.all_posts.clear()
+
+    @classmethod
+    def get_posts(cls,title):
+        posts = Post.query.filter_by(title = title).all()
+        return posts
+
+    @classmethod
+    def get_post(cls,id):
+        post = Post.query.filter_by(id=id).first()
+
+        return post  
--- manage.py
+++ manage.py
@@ -1,4 +1,8 @@
-from app import app
+from app import create_app, db
+from app.models import User
+
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
