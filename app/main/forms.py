95575d62188fad6bb9745ac511111c336a7d3c41
lizKimita <kimita.wanjiku@gmail.com>
2019-04-25 11:53:27 +0300

    adding a new post form and view function

---
+++ app/main/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField, DateField, SelectField
+from wtforms.validators import Required
+
+class PostForm(FlaskForm):
+    post_title = StringField('Post title',validators=[Required()])
+    post = TextAreaField('Post Content', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentsForm(FlaskForm):
+    title = StringField('Post title',validators=[Required()])
+    comment = TextAreaField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,13 @@
-from flask import render_template
-from app import app
+from flask import render_template, request, redirect,url_for,abort
+from . import main
+from ..models import Post, User
+from .forms import PostForm, CommentsForm
+from flask_login import login_required, current_user
+from .. import db,photos
+import markdown2 
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -12,10 +17,29 @@ def index():
     title = "Learn cool things from others"
     return render_template('index.html', title = title)
 
-@app.route('/post/')
+@main.route('/post/')
 def post():
 
     '''
     View post page function that returns the post details page and its data
     '''
     return render_template('post.html')
+
+@main.route('/post/new', methods = ['GET','POST'])
+def new_post():
+    form = PostForm()
+    new_post = None
+
+    if form.validate_on_submit():
+        post_title = form.title.data
+        post = form.post.data
+
+        new_post = Post(post_title = post_title, post = post)
+
+        new_post.save_pitch()
+
+        return redirect(url_for('.index'))
+
+    title = 'New Post'
+    return render_template('new_post.html', title = title, post_form = form, new_post = new_post)
+
