48d4c24ed21bb1659ea12f4abd81785266f5880e
lizKimita <kimita.wanjiku@gmail.com>
2019-04-26 12:53:10 +0300

    adding profile and update profile functionas

--- app/main/views.py
+++ app/main/views.py
@@ -44,6 +44,16 @@ def new_post():
     title = 'New Post'
     return render_template('new_post.html', title = title, post_form = form, new_post = new_post)
 
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+    
+
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
 def update_profile(uname):
