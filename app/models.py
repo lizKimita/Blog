47335b39104f5b67726f5410805715d67c76832e
lizKimita <kimita.wanjiku@gmail.com>
2019-04-28 15:05:20 +0300

    adding a license file

--- app/models.py
+++ app/models.py
@@ -119,3 +119,18 @@ class Comment(db.Model):
         comments = Comment.query.filter_by(post_id=id).all()
 
         return comments
+
+class Email(db.Model):
+    __tablename__='emails'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    email_data = db.Column(db.String(255))
+
+    def save_email(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def send_single_email(cls,id):
+        email = Email.query.filter_by(id=id).first()
+        return email
